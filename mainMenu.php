cbb0212fdd7803cad71ef7b58267f85bb6db1fd7
alvinoPD <alvinoputradika@gmail.com>
2026-03-27 23:47:15 +0700

    fitur hapus

--- mainMenu.php
+++ mainMenu.php
@@ -25,6 +25,7 @@ $query = mysqli_query($db,"SELECT * FROM kegiatan");
             <p><?=$data['detail'];?></p>
             <h5><?=$data['tanggalPengumpulan'];?></h5>
             <h4><?=$data['status'];?></h4>
+            <button><a  href = "../crud/hapus.php?id=<?=$data['id'];?>">selesai</a></button>
         </div>
         <?php }?>
     </main>
