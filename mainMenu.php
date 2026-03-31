f82d299505086502df85a8e78adb91a0e5c89c1c
alvinoPD <alvinoputradika@gmail.com>
2026-03-31 21:49:18 +0700

    add tombol tambah

--- mainMenu.php
+++ mainMenu.php
@@ -9,13 +9,17 @@ $query = mysqli_query($db,"SELECT * FROM kegiatan");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>menu</title>
+    <link rel = "stylesheet" href = "src/mainMenu.css">
+    <link rel = "stylesheet" href = "src/tombolTambah.css">
+    <link rel = "stylesheet" href = "src/nav.css">
 </head>
 <body>
     <header>
 
     </header>
 
+    <?php include "part/nav.php";?>
     <main id = "main">
         <?php while($data = mysqli_fetch_assoc($query)){
 
@@ -25,10 +29,11 @@ $query = mysqli_query($db,"SELECT * FROM kegiatan");
             <p><?=$data['detail'];?></p>
             <h5><?=$data['tanggalPengumpulan'];?></h5>
             <h4><?=$data['status'];?></h4>
-            <button><a  href = "../crud/hapus.php?id=<?=$data['id'];?>">selesai</a></button>
+            <button><a  href = "crud/hapus.php?id=<?=$data['id'];?>">selesai</a></button>
         </div>
         <?php }?>
     </main>
+    <?php include "part/tombolTambah.php";?>
     <footer>
 
     </footer>
